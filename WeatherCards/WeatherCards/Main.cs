8aa63db8ff7e5bb1c26caeabfa388280a26d3a7e
Toleutai Bekzat <toleutaib@gmail.com>
2019-11-28 21:42:21 +0600

    Добавил сущностей

---
+++ WeatherCards/WeatherCards/Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherCards
+{
+    public class Main
+    {
+        public double temp { get; set; }
+        public double temp_min { get; set; }
+        public double temp_max { get; set; }
+        public int pressure { get; set; }
+        public int sea_level { get; set; }
+        public int grnd_level { get; set; }
+        public int humidity { get; set; }
+        public double temp_kf { get; set; }
+    }
+}
