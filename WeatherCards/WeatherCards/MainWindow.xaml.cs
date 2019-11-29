407e9b736698897a57d72747c65a277879ee9d2f
Toleutai Bekzat <toleutaib@gmail.com>
2019-11-29 21:53:36 +0600

    Добавил карточек

--- WeatherCards/WeatherCards/MainWindow.xaml.cs
+++ WeatherCards/WeatherCards/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +22,65 @@ namespace WeatherCards
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string API = "318d86e610dfb76c01702642e1ff63e1";
+        private string url = "https://api.openweathermap.org/data/2.5/forecast?q=*&appid=" + API;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MouseGuidence(object sender, MouseButtonEventArgs e)
+        {
+            searchCity.Clear();
+        }
+
+        private void EnterEvent(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter && !String.IsNullOrWhiteSpace(searchCity.Text))
+            {
+                WebClient webClient = new WebClient();
+
+                url = url.Replace("*", searchCity.Text);
+
+                string weatherData = webClient.DownloadString(url);
+                RootObject root = JsonConvert.DeserializeObject<RootObject>(weatherData);
+
+                DateTime nowDate = DateTime.Now;
+                int i = 0;
+
+                foreach (var item in root.list)
+                {
+
+                    DateTime checkDate = new DateTime();
+                    DateTime.TryParse(item.dt_txt, out checkDate);
+                    if (nowDate.Year == checkDate.Year && nowDate.Month == checkDate.Month && nowDate.Day == checkDate.Day)
+                    {
+                        DateTime checkTime = new DateTime(nowDate.Year, nowDate.Month, nowDate.Day, 21, 0, 0);
+                        if (nowDate.Hour == checkTime.Hour)
+                        {
+                            i++;
+                            nowDate = new DateTime(nowDate.Year, nowDate.Month, nowDate.Day + 1);
+                            foreach (var main in item.weather)
+                            {
+                                if(StreamGeometry.Equals( main.main, "Clouds"))
+                                {
+                                    item.Icon = "Images/Untitled-3.png";
+                                }
+                                else if (StreamGeometry.Equals(main.main, "Clear"))
+                                {
+                                    item.Icon = "Images/Untitled-5.png";
+                                }
+                                else if (StreamGeometry.Equals(main.main, "Rain"))
+                                {
+                                    item.Icon = "Images/Untitled-4.png";
+                                }
+                            }
+                        }
+                    }                    
+                }
+
+                lbWeather.ItemsSource = root.list;
+            }
         }
     }
 }
---
+++ WeatherCards/WeatherCards/RootObject.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherCards
+{
+    public class RootObject
+    {
+        public string cod { get; set; }
+        public int message { get; set; }
+        public int cnt { get; set; }
+        public List<List> list { get; set; }
+        public City city { get; set; }
+    }
+}
